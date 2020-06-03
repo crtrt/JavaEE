7c8c5cd7d7e8079b6d464a2810e19e81498f4562
enovo <17301090@bjtu.eddu.cn>
2020-06-03 11:15:34 +0800

    分离spring cloud

--- JavaEE-SpringMVC/src/main/java/org/example/spring/mvc/service/JdbcService.java
+++ JavaEE-SpringMVC/src/main/java/org/example/spring/mvc/service/JdbcService.java
@@ -2,7 +2,7 @@ package org.example.spring.mvc.service;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
+//控制层之下，dao之上
 @Service
 public class JdbcService {
     public void  getById(Long id){
